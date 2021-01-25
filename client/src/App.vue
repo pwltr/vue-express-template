f4f7992daf9b877e17d60fa5efd68712180f857f
Philipp Walter <philippwalter@pm.me>
2021-01-25 15:07:42 +0100

    build basic UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Morpher Coding Challenge</h1>
+    <Counter />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld,
+    Counter,
   },
 })
 </script>
 
 <style>
 #app {
+  --clr-primary: #00c386;
+  --clr-primary-light: #00d492;
+  --clr-primary-dark: #00aa74;
+  --clr-monochrome-lightest: #ffffff;
+  --clr-monochrome-medium: #363636;
+  --clr-alert: #ee0000;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--clr-monochrome-medium);
   margin-top: 60px;
 }
 </style>
---
+++ client/src/components/Counter.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="Counter">
+    <div class="Counter__number">Current Number: {{ count }}</div>
+    <button class="Counter__button" @click="increment">Increment</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Counter',
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count = this.count + 1
+    },
+  },
+})
+</script>
+
+<style scoped>
+.Counter {
+  padding: 1rem 0;
+}
+
+.Counter__number {
+  font-size: 1.3rem;
+  font-weight: bold;
+  margin: 0.5rem 0 1rem;
+}
+
+.Counter__button {
+  margin-top: 1rem;
+}
+
+button {
+  background: var(--clr-primary);
+  border-color: transparent;
+  border-radius: 5px;
+  border-width: 2px;
+  color: var(--clr-monochrome-lightest);
+  cursor: pointer;
+  font-weight: 700;
+  font-size: 1.125rem;
+  height: 2.875rem;
+  padding: 0 2rem;
+  outline: none;
+}
+
+button:hover {
+  background: var(--clr-primary-light);
+  box-shadow: 0 2px 4px rgb(0 161 111 / 10%), 0 4px 10px rgb(0 161 111 / 20%);
+  transition-delay: 0s;
+  transition-duration: 0.3s;
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.25, 0.46, 0.45, 0.94);
+}
+
+button:active {
+  background: var(--clr-primary-dark);
+}
+</style>
